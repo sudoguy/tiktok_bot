93c9051aa5267daf1f4f9fc5480a8c9712a9819b
Evgeny Kemerov <eskemerov@gmail.com>
2019-09-24 23:05:37 +0400

    Add "list for you feed" bot method and small fixes

--- tiktok_bot/bot/bot.py
+++ tiktok_bot/bot/bot.py
@@ -1,13 +1,30 @@
+from typing import List
+
 from tiktok_bot.api import TikTokAPI
-from tiktok_bot.models.category import ListCategoriesRequest
+from tiktok_bot.models.category import Category, ListCategoriesRequest
+from tiktok_bot.models.feed import ListFeedRequest
+from tiktok_bot.models.feed_enums import FeedType, PullType
+from tiktok_bot.models.post import Post
 
 
 class TikTokBot:
     def __init__(self):
         self.api = TikTokAPI()
 
-    def list_categories(self, count: int = 10, cursor: int = 0):
+    def list_categories(self, count: int = 10, cursor: int = 0) -> List[Category]:
         request = ListCategoriesRequest(count=count, cursor=cursor)
         categories = self.api.list_categories(request)
 
-        return categories
+        return categories.category_list
+
+    def list_for_you_feed(self, count: int = 6, cursor: int = 0) -> List[Post]:
+        request = ListFeedRequest(
+            count=count,
+            max_cursor=cursor,
+            pull_type=PullType.LoadMore,
+            type=FeedType.ForYou,
+            is_cold_start=1,
+        )
+        feed = self.api.list_for_you_feed(request)
+
+        return feed.aweme_list
